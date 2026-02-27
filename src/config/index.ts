e6d026d0a420ce55a5e7682799079852e5d2c0c6
chidi <tyler.igwe@gmail.com>
2026-02-27 11:41:50 +0100

    fix: correct casing of bullMQConfig export in configuration file

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,4 +3,4 @@ import RedisConfig from "./redis.config";
 
 
 export const redisConfig = RedisConfig.getInstance();
-export const bullMqConfig = BullMQConfig.getInstance();
+export const bullMQConfig = BullMQConfig.getInstance();
